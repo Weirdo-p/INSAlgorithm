2f520e6c7e048b8cbf676c4590cae067b44a4fd9
Weirdo <1047325464@qq.com>
2020-09-24 12:40:18 +0800

    build files

---
+++ build/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.dir/src/common.cpp.o"
+  "CMakeFiles/a.dir/src/main.cpp.o"
+  "a.pdb"
+  "a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
